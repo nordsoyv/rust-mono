9e1d7d7570f900247e56a2a14c62fb87eda59c65
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2019-07-02 08:02:40 +0200

    reorg

---
+++ cdl-service/service/src/main.rs
@@ -0,0 +1,13 @@
+use actix_web::{App, HttpServer, Responder, web};
+
+fn index(info: web::Path<(String, u32)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.0, info.1)
+}
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(||
+      App::new()
+        .service(web::resource("/{name}/{id}/index.html").to(index)))
+      .bind("127.0.0.1:8080")?
+      .run()
+}
