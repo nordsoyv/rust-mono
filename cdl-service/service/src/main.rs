8988c866cd76460b3a75b80e316dec72e06556bc
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2019-07-04 15:40:07 +0200

    Parse endpoint

--- cdl-service/service/src/main.rs
+++ cdl-service/service/src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::{App, HttpServer, Responder, web, middleware, HttpResponse};
 use serde_derive::{Deserialize, Serialize};
 use cdl_core::lexer;
+use cdl_core::parser;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct LexRequest {
@@ -23,6 +24,24 @@ fn lex(item: web::Json<LexRequest>) -> HttpResponse {
   };
 }
 
+fn parse(item: web::Json<LexRequest>) -> HttpResponse {
+  let lexer = lexer::Lexer::new();
+  let res = lexer.lex(item.cdl.clone());
+
+  let tokens = match res {
+    Ok(tokens) => tokens,
+    Err(e) => {
+      println!("{:?}", e);
+      return HttpResponse::InternalServerError().finish()
+    }
+  };
+
+  let mut parser = parser::Parser::new();
+  let res = parser.parse(tokens);
+  return HttpResponse::Ok().json(parser);
+
+}
+
 fn main() -> std::io::Result<()> {
   std::env::set_var("RUST_LOG", "actix_web=info");
   env_logger::init();
@@ -32,6 +51,7 @@ fn main() -> std::io::Result<()> {
       .wrap(middleware::Logger::default())
       .data(web::JsonConfig::default().limit(1024 * 100)) // <- limit size of the payload (global configuration)
       .service(web::resource("/lex").route(web::post().to(lex)))
+      .service(web::resource("/parse").route(web::post().to(parse)))
       .service(web::resource("/{name}/{id}/index.html").to(index))
   )
     .bind("127.0.0.1:8080")?
