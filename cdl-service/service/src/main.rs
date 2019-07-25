8490253e7e8f6a8e04d7b2ce99b7c07d6ce97abb
Øyvind <oyvind.nordsveen@gmail.com>
2019-07-25 17:15:40 +0200

    include request stats in response

--- cdl-service/service/src/main.rs
+++ cdl-service/service/src/main.rs
@@ -1,26 +1,60 @@
-use actix_web::{App, HttpServer, Responder, web, middleware, HttpResponse};
+use actix_web::{App, HttpResponse, HttpServer, middleware, Responder, web};
+use log::{error, info};
 use serde_derive::{Deserialize, Serialize};
+
 use cdl_core::lexer;
+use cdl_core::lexer::Token;
 use cdl_core::parser;
-use log::{info, error};
+use cdl_core::parser::Parser;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct LexRequest {
+struct Request {
   cdl: String
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LexResponseStats {
+  lex_time: f64
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ParseResponseStats {
+  lex_time: f64,
+  parse_time: f64,
+  total_time: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LexResponse {
+  stats: LexResponseStats,
+  tokens: Vec<Token>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ParseResponse {
+  stats: ParseResponseStats,
+  ast : Parser
+}
+
 
 fn index(info: web::Path<(String, u32)>) -> impl Responder {
   format!("Hello {}! id:{}", info.0, info.1)
 }
 
-fn lex(item: web::Json<LexRequest>) -> HttpResponse {
+fn lex(item: web::Json<Request>) -> HttpResponse {
   let start = std::time::Instant::now();
   let lexer = lexer::Lexer::new();
   let res = lexer.lex(item.cdl.clone());
   let end = start.elapsed();
-  info!("Time taken to lex : {} milliseconds", (end.as_nanos() as f64) / (1000.0 * 1000.0) );
+  let lex_time = (end.as_nanos() as f64) / (1000.0 * 1000.0);
+  info!("Time taken to lex : {} milliseconds", lex_time);
   return match res {
-    Ok(tokens) => HttpResponse::Ok().json(tokens),
+    Ok(tokens) => HttpResponse::Ok().json(LexResponse {
+      tokens,
+      stats: LexResponseStats {
+        lex_time: lex_time
+      },
+    }),
     Err(e) => {
       error!("{:?}", e);
       HttpResponse::InternalServerError().finish()
@@ -28,28 +62,42 @@ fn lex(item: web::Json<LexRequest>) -> HttpResponse {
   };
 }
 
-fn parse(item: web::Json<LexRequest>) -> HttpResponse {
-  let start = std::time::Instant::now();
+fn parse(item: web::Json<Request>) -> HttpResponse {
+  let start_lex = std::time::Instant::now();
   let lexer = lexer::Lexer::new();
   let res = lexer.lex(item.cdl.clone());
+  let end_lex = start_lex.elapsed();
 
   let tokens = match res {
     Ok(tokens) => tokens,
     Err(e) => {
       error!("{:?}", e);
-      return HttpResponse::InternalServerError().finish()
+      return HttpResponse::InternalServerError().finish();
     }
   };
-
+  let start_parse = std::time::Instant::now();
   let mut parser = parser::Parser::new();
   let res = parser.parse(tokens);
-  let end = start.elapsed();
-  info!("Time taken to lex + parse : {} milliseconds", (end.as_nanos() as f64) / (1000.0 * 1000.0) );
-  match res {
-    Ok(()) =>return HttpResponse::Ok().json(parser),
-    Err(e)=> return HttpResponse::BadRequest().body(e),
-  }
+  let end_parse = start_parse.elapsed();
 
+  let end_total = start_lex.elapsed();
+
+  let lex_time = (end_lex.as_nanos() as f64) / (1000.0 * 1000.0);
+  let parse_time = (end_parse.as_nanos() as f64) / (1000.0 * 1000.0);
+  let total_time = (end_total.as_nanos() as f64) / (1000.0 * 1000.0);
+
+  info!("Time taken to lex + parse : {} milliseconds", total_time);
+  match res {
+    Ok(()) => return HttpResponse::Ok().json(ParseResponse {
+       stats: ParseResponseStats {
+         total_time,
+         lex_time,
+         parse_time,
+       },
+      ast: parser
+    }),
+    Err(e) => return HttpResponse::BadRequest().body(e),
+  }
 }
 
 fn main() -> std::io::Result<()> {
