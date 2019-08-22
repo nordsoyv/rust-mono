bd1a512347e55746aabb2b1a04beb9c83748e60f
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2019-08-22 14:32:25 +0200

    Chapter 11

--- raytracer/src/camera.rs
+++ raytracer/src/camera.rs
@@ -1,6 +1,19 @@
+use serde_derive::{Deserialize, Serialize};
+use rand::distributions::{Uniform, Distribution};
 use crate::vec3::Vec3;
 use crate::ray::Ray;
-use serde_derive::{Deserialize, Serialize};
+
+fn random_in_unit_disk() -> Vec3 {
+  let mut rng = rand::thread_rng();
+  let random = Uniform::from(0.0f32..1.0f32);
+
+  loop {
+    let p = Vec3::new(random.sample(&mut rng), random.sample(&mut rng), 0.0) * 2.0 - Vec3::new(1.0, 1.0, 0.0);
+    if p.dot(p) < 1.0 {
+      return p;
+    }
+  }
+}
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct CameraBuilder {
@@ -9,11 +22,12 @@ pub struct CameraBuilder {
   pub vup: Vec3,
   pub vfov: f32,
   pub aspect: f32,
+  pub aperture : f32,
 }
 
 impl CameraBuilder {
   pub fn build(&self) -> Camera {
-    Camera::new(self.lookfrom, self.lookat, self.vup, self.vfov, self.aspect)
+    Camera::new(self.lookfrom, self.lookat, self.vup, self.vfov, self.aspect, self.aperture)
   }
 }
 
@@ -22,6 +36,10 @@ pub struct Camera {
   pub lower_left_corner: Vec3,
   pub horizontal: Vec3,
   pub vertical: Vec3,
+  pub lens_radius : f32,
+  pub u:Vec3,
+  pub v:Vec3,
+
 }
 
 impl Camera {
@@ -31,10 +49,11 @@ impl Camera {
     vup: Vec3,
     vfov: f32,
     aspect: f32,
-//    aperture: f64,
-//    focus_dist: f64,
+    aperture: f32,
   ) -> Camera {
-//    let lens_radius = aperture / 2.0;
+
+    let focus_dist = (lookfrom-lookat).length();
+    let lens_radius = aperture / 2.0;
     let theta = vfov * std::f32::consts::PI / 180.0;
     let half_height = (theta / 2.0).tan();
     let half_width = aspect * half_height;
@@ -43,50 +62,37 @@ impl Camera {
     let w = dir.to_unit();
     let u = vup.cross(w).to_unit();
     let v = w.cross(u);
-    let lower_left_corner = origin - half_width * u - half_height * v - w;
-    let horizontal = u * half_width * 2.0;
-    let vertical = v * half_height * 2.0;
+    let lower_left_corner = origin - half_width * u * focus_dist - half_height * v * focus_dist - w * focus_dist;
+    let horizontal = u * half_width * 2.0 * focus_dist;
+    let vertical = v * half_height * 2.0 * focus_dist;
     Camera {
       origin,
       horizontal,
       vertical,
       lower_left_corner,
-//      u,
-//      v,
-//      lens_radius,
+      u,
+      v,
+      lens_radius,
     }
   }
 
-  #[allow(dead_code)]
-  pub fn default() -> Camera {
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    Camera {
-      origin,
-      lower_left_corner,
-      horizontal,
-      vertical,
-    }
+//  #[allow(dead_code)]
+//  pub fn default() -> Camera {
+//    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+//    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+//    let vertical = Vec3::new(0.0, 2.0, 0.0);
+//    let origin = Vec3::new(0.0, 0.0, 0.0);
+//    Camera {
+//      origin,
+//      lower_left_corner,
+//      horizontal,
+//      vertical,
+//    }
+//  }
+
+  pub fn get_ray(&self, s: f32, t: f32) -> Ray {
+    let rd = self.lens_radius * random_in_unit_disk();
+    let offset = self.u * rd.x() + self.v* rd.y();
+    Ray::new(self.origin + offset, self.lower_left_corner + (self.horizontal * s) + (self.vertical * t) - self.origin - offset)
   }
-
-  pub fn get_ray(&self, u: f32, v: f32) -> Ray {
-    Ray::new(self.origin, self.lower_left_corner + (self.horizontal * u) + (self.vertical * v) - self.origin)
-  }
-}
-
-
-#[test]
-fn to_json() {
-  let camera = CameraBuilder {
-    lookfrom: Vec3::new(-2.0, 2.0, 1.0),
-    lookat: Vec3::new(0.0, 0.0, -1.0),
-    vup: Vec3::new(0.0, 1.0, 0.0),
-    vfov: 90.0,
-    aspect: 400.0 / 200.0,
-  };
-  let json = r#"{"lookfrom":{"x":-2.0,"y":2.0,"z":1.0},"lookat":{"x":0.0,"y":0.0,"z":-1.0},"vup":{"x":0.0,"y":1.0,"z":0.0},"vfov":90.0,"aspect":2.0}"#.to_string();
-  let s = serde_json::to_string(&camera).unwrap();
-  assert_eq!(s, json);
 }
