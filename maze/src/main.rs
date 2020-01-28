a8f708ecf17db7442c67033b9eca32d54989388c
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2020-01-28 12:58:18 +0100

    Random walls and better line drawing

--- maze/src/main.rs
+++ maze/src/main.rs
@@ -1,4 +1,5 @@
 use minifb::{Window, WindowOptions, Key};
+use rand::distributions::{Uniform, Distribution};
 
 #[derive(Clone, Copy, PartialEq)]
 enum Wall {
@@ -12,26 +13,29 @@ const CELL_HEIGHT: usize = 10;
 const CELL_WIDTH: usize = 10;
 
 struct Canvas {
-  width : usize,
-  height : usize,
-  buffer : Vec<u32>,
+  width: usize,
+  height: usize,
+  buffer: Vec<u32>,
 }
 
 impl Canvas {
-  fn clear (&mut self) {
+  fn clear(&mut self) {
     self.buffer = Vec::new();
     self.buffer.resize(self.width * self.height, 0xff000000);
   }
 
-  fn draw_vert_line(&mut self, start_pos: usize, length: usize) {
+  fn draw_vertical_line(&mut self, start_x: usize, start_y: usize, length: usize) {
+    let margin = 5 * self.width + 5;
+    let top_left = (start_y * self.width) + (start_x) + margin;
     for pos in 0..length {
-      self.buffer[start_pos + (pos * self.width)] = 0x00ffffff;
+      self.buffer[top_left + (pos * self.width)] = 0x00ffffff;
     }
   }
-
-  fn draw_horz_line(&mut self,  start_pos: usize, length: usize) {
+  fn draw_horizontal_line(&mut self, start_x: usize, start_y: usize, length: usize) {
+    let margin = 5 * self.width + 5;
+    let top_left = (start_y * self.width) + (start_x) + margin;
     for pos in 0..length {
-      self.buffer[start_pos + pos] = 0x00ffffff;
+      self.buffer[top_left + pos] = 0x00ffffff;
     }
   }
 }
@@ -51,21 +55,30 @@ struct Cell {
 }
 
 impl Cell {
-  fn draw(&self, canvas: &mut Canvas, top_left: usize) {
+  fn draw(&self, canvas: &mut Canvas) {
     if self.top == Wall::Wall {
-      canvas.draw_horz_line( top_left, CELL_WIDTH);
+      canvas.draw_horizontal_line(self.x_pos * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_WIDTH);
     }
     if self.bottom == Wall::Wall {
-      canvas.draw_horz_line( top_left + CELL_WIDTH * WIDTH, CELL_WIDTH);
+      canvas.draw_horizontal_line(self.x_pos * CELL_WIDTH, (self.y_pos + 1) * CELL_HEIGHT, CELL_WIDTH);
     }
     if self.left == Wall::Wall {
-      canvas.draw_vert_line( top_left, CELL_HEIGHT);
+      canvas.draw_vertical_line(self.x_pos * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_HEIGHT);
     }
     if self.right == Wall::Wall {
-      canvas.draw_vert_line( top_left + CELL_WIDTH, CELL_HEIGHT);
+      canvas.draw_vertical_line((self.x_pos + 1) * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_HEIGHT);
     }
   }
+}
 
+fn random_wall() -> Wall {
+  let mut rng = rand::thread_rng();
+  let random = Uniform::from(0..10);
+  if random.sample(&mut rng) < 7 {
+    Wall::None
+  }else {
+    Wall::Wall
+  }
 }
 
 impl Maze {
@@ -75,10 +88,10 @@ impl Maze {
     for x in 0..79 {
       for y in 0..79 {
         cells.push(Cell {
-          bottom: Wall::Wall,
-          left: Wall::Wall,
-          top: Wall::Wall,
-          right: Wall::Wall,
+          bottom: random_wall(),
+          left: random_wall(),
+          top: random_wall(),
+          right: random_wall(),
           x_pos: x,
           y_pos: y,
         });
@@ -89,6 +102,8 @@ impl Maze {
     }
   }
 
+
+
   fn draw(&self, canvas: &mut Canvas) {
     for cell in &self.cells {
       self.draw_cell(canvas, *cell);
@@ -96,9 +111,7 @@ impl Maze {
   }
 
   fn draw_cell(&self, canvas: &mut Canvas, cell: Cell) {
-    let margin = 5 * WIDTH + 5;
-    let top_left = (cell.y_pos * CELL_HEIGHT * WIDTH) + (cell.x_pos * CELL_WIDTH) + margin;
-    cell.draw(canvas, top_left);
+    cell.draw(canvas);
   }
 }
 
@@ -120,9 +133,9 @@ fn main() {
 //      let mut buffer: Vec<u32> = Vec::new();
 //      buffer.resize(WIDTH * HEIGHT, 0xff000000);
       let mut canvas = Canvas {
-        width : WIDTH,
-        height : HEIGHT,
-        buffer: vec![]
+        width: WIDTH,
+        height: HEIGHT,
+        buffer: vec![],
       };
       canvas.clear();
       maze.draw(&mut canvas);
