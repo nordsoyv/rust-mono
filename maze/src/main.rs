d2537610e312fbc068d30a80a9d9e2bd61d99670
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2020-01-30 08:46:18 +0100

    Better maze handling

--- maze/src/main.rs
+++ maze/src/main.rs
@@ -1,16 +1,20 @@
 use minifb::{Window, WindowOptions, Key};
 use rand::distributions::{Uniform, Distribution};
+use std::env::var;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 enum Wall {
   None,
   Wall,
 }
 
-const WIDTH: usize = 800;
-const HEIGHT: usize = 800;
+const WIDTH: usize = 810;
+const HEIGHT: usize = 810;
 const CELL_HEIGHT: usize = 10;
 const CELL_WIDTH: usize = 10;
+const NUM_CELLS: usize = 80;
+const BACKGROUND_COLOR: u32 = 0x00ffffff;
+const FOREGROUND_COLOR: u32 = 0xff000000;
 
 struct Canvas {
   width: usize,
@@ -21,21 +25,21 @@ struct Canvas {
 impl Canvas {
   fn clear(&mut self) {
     self.buffer = Vec::new();
-    self.buffer.resize(self.width * self.height, 0xff000000);
+    self.buffer.resize(self.width * self.height, BACKGROUND_COLOR);
   }
 
   fn draw_vertical_line(&mut self, start_x: usize, start_y: usize, length: usize) {
     let margin = 5 * self.width + 5;
     let top_left = (start_y * self.width) + (start_x) + margin;
     for pos in 0..length {
-      self.buffer[top_left + (pos * self.width)] = 0x00ffffff;
+      self.buffer[top_left + (pos * self.width)] = FOREGROUND_COLOR;
     }
   }
   fn draw_horizontal_line(&mut self, start_x: usize, start_y: usize, length: usize) {
     let margin = 5 * self.width + 5;
     let top_left = (start_y * self.width) + (start_x) + margin;
     for pos in 0..length {
-      self.buffer[top_left + pos] = 0x00ffffff;
+      self.buffer[top_left + pos] = FOREGROUND_COLOR;
     }
   }
 }
@@ -44,7 +48,7 @@ struct Maze {
   cells: Vec<Cell>,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Cell {
   pub left: Wall,
   pub right: Wall,
@@ -76,33 +80,47 @@ fn random_wall() -> Wall {
   let random = Uniform::from(0..10);
   if random.sample(&mut rng) < 7 {
     Wall::None
-  }else {
+  } else {
     Wall::Wall
   }
 }
 
 impl Maze {
   fn new() -> Maze {
-    let mut cells = vec![];
+    Maze {
+      cells: vec![]
+    }
+  }
 
-    for x in 0..79 {
-      for y in 0..79 {
-        cells.push(Cell {
-          bottom: random_wall(),
-          left: random_wall(),
-          top: random_wall(),
-          right: random_wall(),
+  fn get_cell(&self, x: usize, y: usize) -> &Cell {
+    let index = y * NUM_CELLS + x;
+    return &self.cells[index];
+  }
+
+  fn get_mut_cell(&mut self, x: usize, y: usize) -> &mut Cell {
+    let index = y * NUM_CELLS + x;
+    return &mut self.cells[index];
+  }
+
+  fn generate(&mut self) {
+    for y in 0..NUM_CELLS {
+      for x in 0..NUM_CELLS {
+        self.cells.push(Cell {
+          bottom: Wall::Wall,
+          left: Wall::Wall,
+          top: Wall::Wall,
+          right: Wall::Wall,
           x_pos: x,
           y_pos: y,
         });
       }
     }
-    Maze {
-      cells
-    }
+
+    let mut start = self.get_mut_cell(5,0);
+    start.top = Wall::None;
+    let mut end = self.get_mut_cell(5,79);
+    end.bottom = Wall::None;
   }
-
-
 
   fn draw(&self, canvas: &mut Canvas) {
     for cell in &self.cells {
@@ -126,12 +144,11 @@ fn main() {
   });
 
 
-  let maze = Maze::new();
+  let mut maze = Maze::new();
+  maze.generate();
 
   while window.is_open() && !window.is_key_down(Key::Escape) {
     {
-//      let mut buffer: Vec<u32> = Vec::new();
-//      buffer.resize(WIDTH * HEIGHT, 0xff000000);
       let mut canvas = Canvas {
         width: WIDTH,
         height: HEIGHT,
