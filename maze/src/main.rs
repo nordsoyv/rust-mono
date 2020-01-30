d160e35f85846abb2fd480f46925389f1365741d
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2020-01-30 11:26:39 +0100

    get direction and cleanup

--- maze/src/main.rs
+++ maze/src/main.rs
@@ -1,6 +1,15 @@
 use minifb::{Window, WindowOptions, Key};
 use rand::distributions::{Uniform, Distribution};
-use std::env::var;
+use std::convert::TryFrom;
+use rand::prelude::ThreadRng;
+
+const WIDTH: i32 = 810;
+const HEIGHT: i32 = 810;
+const CELL_HEIGHT: i32 = 10;
+const CELL_WIDTH: i32 = 10;
+const NUM_CELLS: i32 = 80;
+const BACKGROUND_COLOR: u32 = 0x00ffffff;
+const FOREGROUND_COLOR: u32 = 0xff000000;
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 enum Wall {
@@ -16,14 +25,6 @@ enum Direction {
   West,
 }
 
-const WIDTH: i32 = 810;
-const HEIGHT: i32 = 810;
-const CELL_HEIGHT: i32 = 10;
-const CELL_WIDTH: i32 = 10;
-const NUM_CELLS: i32 = 80;
-const BACKGROUND_COLOR: u32 = 0x00ffffff;
-const FOREGROUND_COLOR: u32 = 0xff000000;
-
 struct Canvas {
   width: i32,
   height: i32,
@@ -33,70 +34,58 @@ struct Canvas {
 impl Canvas {
   fn clear(&mut self) {
     self.buffer = Vec::new();
-    self.buffer.resize((self.width * self.height) as usize, BACKGROUND_COLOR);
+    let size = usize::try_from(self.width * self.height).unwrap();
+    self.buffer.resize(size, BACKGROUND_COLOR);
   }
 
   fn draw_vertical_line(&mut self, start_x: i32, start_y: i32, length: i32) {
+    assert!(length > 0);
+    assert!(start_x >= 0);
+    assert!(start_y >= 0);
     let margin = 5 * self.width + 5;
     let top_left = (start_y * self.width) + (start_x) + margin;
     for pos in 0..length {
-      self.buffer[(top_left + (pos * self.width))as usize ] = FOREGROUND_COLOR;
+      self.buffer[(top_left + (pos * self.width)) as usize] = FOREGROUND_COLOR;
     }
   }
   fn draw_horizontal_line(&mut self, start_x: i32, start_y: i32, length: i32) {
+    assert!(length > 0);
+    assert!(start_x >= 0);
+    assert!(start_y >= 0);
     let margin = 5 * self.width + 5;
     let top_left = (start_y * self.width) + (start_x) + margin;
     for pos in 0..length {
-      self.buffer[(top_left + pos)as usize] = FOREGROUND_COLOR;
+      self.buffer[(top_left + pos) as usize] = FOREGROUND_COLOR;
     }
   }
 }
 
 struct Maze {
   cells: Vec<Cell>,
-}
-
-#[derive(Clone, Copy, Debug)]
-struct Cell {
-  pub left: Wall,
-  pub right: Wall,
-  pub top: Wall,
-  pub bottom: Wall,
-  pub x_pos: i32,
-  pub y_pos: i32,
-}
-
-impl Cell {
-  fn draw(&self, canvas: &mut Canvas) {
-    if self.top == Wall::Wall {
-      canvas.draw_horizontal_line(self.x_pos * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_WIDTH);
-    }
-    if self.bottom == Wall::Wall {
-      canvas.draw_horizontal_line(self.x_pos * CELL_WIDTH, (self.y_pos + 1) * CELL_HEIGHT, CELL_WIDTH);
-    }
-    if self.left == Wall::Wall {
-      canvas.draw_vertical_line(self.x_pos * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_HEIGHT);
-    }
-    if self.right == Wall::Wall {
-      canvas.draw_vertical_line((self.x_pos + 1) * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_HEIGHT);
-    }
-  }
-}
-
-fn random_wall() -> Wall {
-  let mut rng = rand::thread_rng();
-  let random = Uniform::from(0..10);
-  if random.sample(&mut rng) < 7 {
-    Wall::None
-  } else {
-    Wall::Wall
-  }
+  rng: ThreadRng,
+  random: Uniform<f32>,
 }
 
 impl Maze {
   fn new() -> Maze {
     Maze {
-      cells: vec![]
+      rng: rand::thread_rng(),
+      random: Uniform::from(0.0f32..1.0f32),
+      cells: vec![],
+    }
+  }
+
+  fn get_direction(&mut self) -> Direction {
+    let mut d = self.random.sample(&mut self.rng);
+    d = d * 4.0f32;
+    if d < 1.0f32 {
+      Direction::North
+    } else if d < 2.0f32 {
+      Direction::East
+    } else if d < 3.0f32 {
+      Direction::South
+    } else {
+      Direction::West
     }
   }
 
@@ -198,12 +187,51 @@ impl Maze {
   }
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Cell {
+  pub left: Wall,
+  pub right: Wall,
+  pub top: Wall,
+  pub bottom: Wall,
+  pub x_pos: i32,
+  pub y_pos: i32,
+}
+
+impl Cell {
+  fn draw(&self, canvas: &mut Canvas) {
+    if self.top == Wall::Wall {
+      canvas.draw_horizontal_line(self.x_pos * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_WIDTH);
+    }
+    if self.bottom == Wall::Wall {
+      canvas.draw_horizontal_line(self.x_pos * CELL_WIDTH, (self.y_pos + 1) * CELL_HEIGHT, CELL_WIDTH);
+    }
+    if self.left == Wall::Wall {
+      canvas.draw_vertical_line(self.x_pos * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_HEIGHT);
+    }
+    if self.right == Wall::Wall {
+      canvas.draw_vertical_line((self.x_pos + 1) * CELL_WIDTH, self.y_pos * CELL_HEIGHT, CELL_HEIGHT);
+    }
+  }
+}
+
+/*
+fn random_wall() -> Wall {
+  let mut rng = rand::thread_rng();
+  let random = Uniform::from(0..10);
+  if random.sample(&mut rng) < 7 {
+    Wall::None
+  } else {
+    Wall::Wall
+  }
+}
+*/
+
 
 fn main() {
   let mut window = Window::new(
     "Test - ESC to exit",
-    WIDTH as usize,
-    HEIGHT as usize,
+    usize::try_from(WIDTH).unwrap(),
+    usize::try_from(HEIGHT).unwrap(),
     WindowOptions::default()).unwrap_or_else(|e| {
     panic!("{}", e);
   });
@@ -211,8 +239,7 @@ fn main() {
 
   let mut maze = Maze::new();
   maze.generate();
-  maze.carve(10,1,Direction::South);
-
+  maze.carve(10, 1, Direction::South);
   while window.is_open() && !window.is_key_down(Key::Escape) {
     {
       let mut canvas = Canvas {
