5493d38533aa2d0108ae99ef08f9f152f9617beb
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2020-12-22 16:13:45 +0100

    Task 03

--- advent/src/a_of_c_2020/mod.rs
+++ advent/src/a_of_c_2020/mod.rs
@@ -1,9 +1,11 @@
 use crate::task::Task;
 use crate::a_of_c_2020::task01::{Task01A, Task01B};
 use crate::a_of_c_2020::task02::{Task02B, Task02A};
+use crate::a_of_c_2020::task03::{Task03A, Task03B};
 
 mod task01;
 mod task02;
+mod task03;
 
 pub fn create_2020_task(id: &str) -> Box<dyn Task> {
   match id {
@@ -18,6 +20,12 @@ pub fn create_2020_task(id: &str) -> Box<dyn Task> {
     }
     "02b" => {
       Box::new(Task02B {})
+    }
+    "03a" => {
+      Box::new(Task03A {})
+    }
+    "03b" => {
+      Box::new(Task03B {})
     }
     _ => panic!("No task found"),
   }
