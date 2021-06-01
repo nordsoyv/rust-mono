3e48a58cffacb179618dbc1b6d5e941529fa8bf2
Øyvind <oyvind.nordsveen@gmail.com>
2021-06-02 01:11:30 +0200

    continue  of wgpu experiments

--- wgpu-experiments/src/main.rs
+++ wgpu-experiments/src/main.rs
@@ -1,10 +1,67 @@
-use futures::executor::block_on;
+use std::iter;
+
+use wgpu::util::DeviceExt;
 use winit::{
   event::*,
   event_loop::{ControlFlow, EventLoop},
-  window::Window,
-  window::WindowBuilder,
+  window::{Window, WindowBuilder},
 };
+
+mod texture;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+  position: [f32; 3],
+  tex_coords: [f32; 2],
+}
+
+impl Vertex {
+  fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+    use std::mem;
+    wgpu::VertexBufferLayout {
+      array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+      step_mode: wgpu::InputStepMode::Vertex,
+      attributes: &[
+        wgpu::VertexAttribute {
+          offset: 0,
+          shader_location: 0,
+          format: wgpu::VertexFormat::Float32x3,
+        },
+        wgpu::VertexAttribute {
+          offset: mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+          shader_location: 1,
+          format: wgpu::VertexFormat::Float32x2,
+        },
+      ],
+    }
+  }
+}
+
+const VERTICES: &[Vertex] = &[
+  Vertex {
+    position: [-0.0868241, 0.49240386, 0.0],
+    tex_coords: [0.4131759, 0.00759614],
+  }, // A
+  Vertex {
+    position: [-0.49513406, 0.06958647, 0.0],
+    tex_coords: [0.0048659444, 0.43041354],
+  }, // B
+  Vertex {
+    position: [-0.21918549, -0.44939706, 0.0],
+    tex_coords: [0.28081453, 0.949397057],
+  }, // C
+  Vertex {
+    position: [0.35966998, -0.3473291, 0.0],
+    tex_coords: [0.85967, 0.84732911],
+  }, // D
+  Vertex {
+    position: [0.44147372, 0.2347359, 0.0],
+    tex_coords: [0.9414737, 0.2652641],
+  }, // E
+];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
 
 struct State {
   surface: wgpu::Surface,
@@ -13,10 +70,17 @@ struct State {
   sc_desc: wgpu::SwapChainDescriptor,
   swap_chain: wgpu::SwapChain,
   size: winit::dpi::PhysicalSize<u32>,
+  render_pipeline: wgpu::RenderPipeline,
+  vertex_buffer: wgpu::Buffer,
+  index_buffer: wgpu::Buffer,
+  num_indices: u32,
+  // NEW!
+  #[allow(dead_code)]
+  diffuse_texture: texture::Texture,
+  diffuse_bind_group: wgpu::BindGroup,
 }
 
 impl State {
-  // Creating some of the wgpu types requires async code
   async fn new(window: &Window) -> Self {
     let size = window.inner_size();
 
@@ -34,14 +98,15 @@ impl State {
     let (device, queue) = adapter
       .request_device(
         &wgpu::DeviceDescriptor {
+          label: None,
           features: wgpu::Features::empty(),
           limits: wgpu::Limits::default(),
-          label: None,
         },
         None, // Trace path
       )
       .await
       .unwrap();
+
     let sc_desc = wgpu::SwapChainDescriptor {
       usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
       format: adapter.get_swap_chain_preferred_format(&surface).unwrap(),
@@ -50,6 +115,116 @@ impl State {
       present_mode: wgpu::PresentMode::Fifo,
     };
     let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+    let diffuse_bytes = include_bytes!("happy-tree.png");
+    let diffuse_texture =
+      texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "happy-tree.png").unwrap();
+
+    let texture_bind_group_layout =
+      device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        entries: &[
+          wgpu::BindGroupLayoutEntry {
+            binding: 0,
+            visibility: wgpu::ShaderStage::FRAGMENT,
+            ty: wgpu::BindingType::Texture {
+              multisampled: false,
+              view_dimension: wgpu::TextureViewDimension::D2,
+              sample_type: wgpu::TextureSampleType::Float { filterable: true },
+            },
+            count: None,
+          },
+          wgpu::BindGroupLayoutEntry {
+            binding: 1,
+            visibility: wgpu::ShaderStage::FRAGMENT,
+            ty: wgpu::BindingType::Sampler {
+              comparison: false,
+              filtering: true,
+            },
+            count: None,
+          },
+        ],
+        label: Some("texture_bind_group_layout"),
+      });
+
+    let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+      layout: &texture_bind_group_layout,
+      entries: &[
+        wgpu::BindGroupEntry {
+          binding: 0,
+          resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
+        },
+        wgpu::BindGroupEntry {
+          binding: 1,
+          resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler),
+        },
+      ],
+      label: Some("diffuse_bind_group"),
+    });
+
+    let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+      label: Some("Shader"),
+      flags: wgpu::ShaderFlags::all(),
+      source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+    });
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+      label: Some("Render Pipeline Layout"),
+      bind_group_layouts: &[&texture_bind_group_layout],
+      push_constant_ranges: &[],
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+      label: Some("Render Pipeline"),
+      layout: Some(&render_pipeline_layout),
+      vertex: wgpu::VertexState {
+        module: &shader,
+        entry_point: "main",
+        buffers: &[Vertex::desc()],
+      },
+      fragment: Some(wgpu::FragmentState {
+        module: &shader,
+        entry_point: "main",
+        targets: &[wgpu::ColorTargetState {
+          format: sc_desc.format,
+          blend: Some(wgpu::BlendState {
+            color: wgpu::BlendComponent::REPLACE,
+            alpha: wgpu::BlendComponent::REPLACE,
+          }),
+          write_mask: wgpu::ColorWrite::ALL,
+        }],
+      }),
+      primitive: wgpu::PrimitiveState {
+        topology: wgpu::PrimitiveTopology::TriangleList,
+        strip_index_format: None,
+        front_face: wgpu::FrontFace::Ccw,
+        cull_mode: Some(wgpu::Face::Back),
+        // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+        polygon_mode: wgpu::PolygonMode::Fill,
+        // Requires Features::DEPTH_CLAMPING
+        clamp_depth: false,
+        // Requires Features::CONSERVATIVE_RASTERIZATION
+        conservative: false,
+      },
+      depth_stencil: None,
+      multisample: wgpu::MultisampleState {
+        count: 1,
+        mask: !0,
+        alpha_to_coverage_enabled: false,
+      },
+    });
+
+    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+      label: Some("Vertex Buffer"),
+      contents: bytemuck::cast_slice(VERTICES),
+      usage: wgpu::BufferUsage::VERTEX,
+    });
+    let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+      label: Some("Index Buffer"),
+      contents: bytemuck::cast_slice(INDICES),
+      usage: wgpu::BufferUsage::INDEX,
+    });
+    let num_indices = INDICES.len() as u32;
+
     Self {
       surface,
       device,
@@ -57,6 +232,12 @@ impl State {
       sc_desc,
       swap_chain,
       size,
+      render_pipeline,
+      vertex_buffer,
+      index_buffer,
+      num_indices,
+      diffuse_texture,
+      diffuse_bind_group,
     }
   }
 
@@ -67,23 +248,24 @@ impl State {
     self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
   }
 
+  #[allow(unused_variables)]
   fn input(&mut self, event: &WindowEvent) -> bool {
     false
   }
 
-  fn update(&mut self) {
-    // todo!()
-  }
+  fn update(&mut self) {}
 
   fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
     let frame = self.swap_chain.get_current_frame()?.output;
+
     let mut encoder = self
       .device
       .create_command_encoder(&wgpu::CommandEncoderDescriptor {
         label: Some("Render Encoder"),
       });
+
     {
-      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+      let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
         label: Some("Render Pass"),
         color_attachments: &[wgpu::RenderPassColorAttachment {
           view: &frame.view,
@@ -100,10 +282,15 @@ impl State {
         }],
         depth_stencil_attachment: None,
       });
+
+      render_pass.set_pipeline(&self.render_pipeline);
+      render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+      render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+      render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+      render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
     }
 
-    // submit will accept anything that implements IntoIter
-    self.queue.submit(std::iter::once(encoder.finish()));
+    self.queue.submit(iter::once(encoder.finish()));
 
     Ok(())
   }
@@ -113,50 +300,58 @@ fn main() {
   env_logger::init();
   let event_loop = EventLoop::new();
   let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+  use futures::executor::block_on;
+
   // Since main can't be async, we're going to need to block
   let mut state = block_on(State::new(&window));
 
-  event_loop.run(move |event, _, control_flow| match event {
-    Event::WindowEvent {
-      ref event,
-      window_id,
-    } if window_id == window.id() => match event {
-      WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-      WindowEvent::KeyboardInput { input, .. } => match input {
-        KeyboardInput {
-          state: ElementState::Pressed,
-          virtual_keycode: Some(VirtualKeyCode::Escape),
-          ..
-        } => *control_flow = ControlFlow::Exit,
-        _ => {}
-      },
-      WindowEvent::Resized(physical_size) => {
-        state.resize(*physical_size);
+  event_loop.run(move |event, _, control_flow| {
+    match event {
+      Event::WindowEvent {
+        ref event,
+        window_id,
+      } if window_id == window.id() => {
+        if !state.input(event) {
+          match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::KeyboardInput { input, .. } => match input {
+              KeyboardInput {
+                state: ElementState::Pressed,
+                virtual_keycode: Some(VirtualKeyCode::Escape),
+                ..
+              } => *control_flow = ControlFlow::Exit,
+              _ => {}
+            },
+            WindowEvent::Resized(physical_size) => {
+              state.resize(*physical_size);
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+              // new_inner_size is &mut so w have to dereference it twice
+              state.resize(**new_inner_size);
+            }
+            _ => {}
+          }
+        }
       }
-      WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-        // new_inner_size is &&mut so we have to dereference it twice
-        state.resize(**new_inner_size);
+      Event::RedrawRequested(_) => {
+        state.update();
+        match state.render() {
+          Ok(_) => {}
+          // Recreate the swap_chain if lost
+          Err(wgpu::SwapChainError::Lost) => state.resize(state.size),
+          // The system is out of memory, we should probably quit
+          Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+          // All other errors (Outdated, Timeout) should be resolved by the next frame
+          Err(e) => eprintln!("{:?}", e),
+        }
+      }
+      Event::MainEventsCleared => {
+        // RedrawRequested will only trigger once, unless we manually
+        // request it.
+        window.request_redraw();
       }
       _ => {}
-    },
-    Event::RedrawRequested(_) => {
-      state.update();
-      match state.render() {
-        Ok(_) => {}
-        // Recreate the swap_chain if lost
-        Err(wgpu::SwapChainError::Lost) => state.resize(state.size),
-        // The system is out of memory, we should probably quit
-        Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-        // All other errors (Outdated, Timeout) should be resolved by the next frame
-        Err(e) => eprintln!("{:?}", e),
-      }
     }
-    Event::MainEventsCleared => {
-      // RedrawRequested will only trigger once, unless we manually
-      // request it.
-      window.request_redraw();
-    }
-
-    _ => {}
   });
 }
