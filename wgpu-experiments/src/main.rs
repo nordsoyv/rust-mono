4c67d2cf79bd57f61264538d5738ba156d06cead
Øyvind <oyvind.nordsveen@gmail.com>
2021-06-02 20:30:45 +0200

    uniform buffer

--- wgpu-experiments/src/main.rs
+++ wgpu-experiments/src/main.rs
@@ -63,6 +63,59 @@ const VERTICES: &[Vertex] = &[
 
 const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
 
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+  1.0, 0.0, 0.0, 0.0,
+  0.0, 1.0, 0.0, 0.0,
+  0.0, 0.0, 0.5, 0.0,
+  0.0, 0.0, 0.5, 1.0,
+);
+
+struct Camera {
+  eye: cgmath::Point3<f32>,
+  target: cgmath::Point3<f32>,
+  up: cgmath::Vector3<f32>,
+  aspect: f32,
+  fovy: f32,
+  znear: f32,
+  zfar: f32,
+}
+
+impl Camera {
+  fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+    // 1.
+    let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+    // 2.
+    let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+
+    // 3.
+    return OPENGL_TO_WGPU_MATRIX * proj * view;
+  }
+}
+
+// We need this for Rust to store our data correctly for the shaders
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct Uniforms {
+  // We can't use cgmath with bytemuck directly so we'll have
+  // to convert the Matrix4 into a 4x4 f32 array
+  view_proj: [[f32; 4]; 4],
+}
+
+impl Uniforms {
+  fn new() -> Self {
+    use cgmath::SquareMatrix;
+    Self {
+      view_proj: cgmath::Matrix4::identity().into(),
+    }
+  }
+
+  fn update_view_proj(&mut self, camera: &Camera) {
+    self.view_proj = camera.build_view_projection_matrix().into();
+  }
+}
+
 struct State {
   surface: wgpu::Surface,
   device: wgpu::Device,
@@ -78,11 +131,18 @@ struct State {
   #[allow(dead_code)]
   diffuse_texture: texture::Texture,
   diffuse_bind_group: wgpu::BindGroup,
+  camera: Camera,
+  camera_controller: CameraController,
+  uniforms: Uniforms,
+  uniform_buffer: wgpu::Buffer,
+  uniform_bind_group: wgpu::BindGroup,
 }
 
 impl State {
   async fn new(window: &Window) -> Self {
     let size = window.inner_size();
+
+    let camera_controller = CameraController::new(0.2);
 
     // The instance is a handle to our GPU
     // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
@@ -160,6 +220,51 @@ impl State {
       ],
       label: Some("diffuse_bind_group"),
     });
+    let camera = Camera {
+      // position the camera one unit up and 2 units back
+      // +z is out of the screen
+      eye: (0.0, 1.0, 2.0).into(),
+      // have it look at the origin
+      target: (0.0, 0.0, 0.0).into(),
+      // which way is "up"
+      up: cgmath::Vector3::unit_y(),
+      aspect: sc_desc.width as f32 / sc_desc.height as f32,
+      fovy: 45.0,
+      znear: 0.1,
+      zfar: 100.0,
+    };
+    let mut uniforms = Uniforms::new();
+    uniforms.update_view_proj(&camera);
+
+    let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+      label: Some("Uniform Buffer"),
+      contents: bytemuck::cast_slice(&[uniforms]),
+      usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+    });
+
+    let uniform_bind_group_layout =
+      device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        entries: &[wgpu::BindGroupLayoutEntry {
+          binding: 0,
+          visibility: wgpu::ShaderStage::VERTEX,
+          ty: wgpu::BindingType::Buffer {
+            ty: wgpu::BufferBindingType::Uniform,
+            has_dynamic_offset: false,
+            min_binding_size: None,
+          },
+          count: None,
+        }],
+        label: Some("uniform_bind_group_layout"),
+      });
+
+    let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+      layout: &uniform_bind_group_layout,
+      entries: &[wgpu::BindGroupEntry {
+        binding: 0,
+        resource: uniform_buffer.as_entire_binding(),
+      }],
+      label: Some("uniform_bind_group"),
+    });
 
     let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
       label: Some("Shader"),
@@ -169,7 +274,7 @@ impl State {
 
     let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
       label: Some("Render Pipeline Layout"),
-      bind_group_layouts: &[&texture_bind_group_layout],
+      bind_group_layouts: &[&texture_bind_group_layout, &uniform_bind_group_layout],
       push_constant_ranges: &[],
     });
 
@@ -238,6 +343,11 @@ impl State {
       num_indices,
       diffuse_texture,
       diffuse_bind_group,
+      camera,
+      camera_controller,
+      uniforms,
+      uniform_buffer,
+      uniform_bind_group,
     }
   }
 
@@ -250,10 +360,18 @@ impl State {
 
   #[allow(unused_variables)]
   fn input(&mut self, event: &WindowEvent) -> bool {
-    false
+    self.camera_controller.process_events(event)
   }
 
-  fn update(&mut self) {}
+  fn update(&mut self) {
+    self.camera_controller.update_camera(&mut self.camera);
+    self.uniforms.update_view_proj(&self.camera);
+    self.queue.write_buffer(
+      &self.uniform_buffer,
+      0,
+      bytemuck::cast_slice(&[self.uniforms]),
+    );
+  }
 
   fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
     let frame = self.swap_chain.get_current_frame()?.output;
@@ -285,6 +403,7 @@ impl State {
 
       render_pass.set_pipeline(&self.render_pipeline);
       render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+      render_pass.set_bind_group(1, &self.uniform_bind_group, &[]);
       render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
       render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
       render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
@@ -293,6 +412,106 @@ impl State {
     self.queue.submit(iter::once(encoder.finish()));
 
     Ok(())
+  }
+}
+
+struct CameraController {
+  speed: f32,
+  is_up_pressed: bool,
+  is_down_pressed: bool,
+  is_forward_pressed: bool,
+  is_backward_pressed: bool,
+  is_left_pressed: bool,
+  is_right_pressed: bool,
+}
+
+impl CameraController {
+  fn new(speed: f32) -> Self {
+    Self {
+      speed,
+      is_up_pressed: false,
+      is_down_pressed: false,
+      is_forward_pressed: false,
+      is_backward_pressed: false,
+      is_left_pressed: false,
+      is_right_pressed: false,
+    }
+  }
+
+  fn process_events(&mut self, event: &WindowEvent) -> bool {
+    match event {
+      WindowEvent::KeyboardInput {
+        input:
+          KeyboardInput {
+            state,
+            virtual_keycode: Some(keycode),
+            ..
+          },
+        ..
+      } => {
+        let is_pressed = *state == ElementState::Pressed;
+        match keycode {
+          VirtualKeyCode::Space => {
+            self.is_up_pressed = is_pressed;
+            true
+          }
+          VirtualKeyCode::LShift => {
+            self.is_down_pressed = is_pressed;
+            true
+          }
+          VirtualKeyCode::W | VirtualKeyCode::Up => {
+            self.is_forward_pressed = is_pressed;
+            true
+          }
+          VirtualKeyCode::A | VirtualKeyCode::Left => {
+            self.is_left_pressed = is_pressed;
+            true
+          }
+          VirtualKeyCode::S | VirtualKeyCode::Down => {
+            self.is_backward_pressed = is_pressed;
+            true
+          }
+          VirtualKeyCode::D | VirtualKeyCode::Right => {
+            self.is_right_pressed = is_pressed;
+            true
+          }
+          _ => false,
+        }
+      }
+      _ => false,
+    }
+  }
+
+  fn update_camera(&self, camera: &mut Camera) {
+    use cgmath::InnerSpace;
+    let forward = camera.target - camera.eye;
+    let forward_norm = forward.normalize();
+    let forward_mag = forward.magnitude();
+
+    // Prevents glitching when camera gets too close to the
+    // center of the scene.
+    if self.is_forward_pressed && forward_mag > self.speed {
+      camera.eye += forward_norm * self.speed;
+    }
+    if self.is_backward_pressed {
+      camera.eye -= forward_norm * self.speed;
+    }
+
+    let right = forward_norm.cross(camera.up);
+
+    // Redo radius calc in case the up/ down is pressed.
+    let forward = camera.target - camera.eye;
+    let forward_mag = forward.magnitude();
+
+    if self.is_right_pressed {
+      // Rescale the distance between the target and eye so
+      // that it doesn't change. The eye therefore still
+      // lies on the circle made by the target and eye.
+      camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
+    }
+    if self.is_left_pressed {
+      camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+    }
   }
 }
 
