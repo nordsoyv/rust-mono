ee02fa565300ce51c2366dec3c61d3c185c85773
Øyvind <oyvind.nordsveen@gmail.com>
2021-06-03 08:44:13 +0200

    instances

--- wgpu-experiments/src/main.rs
+++ wgpu-experiments/src/main.rs
@@ -1,5 +1,6 @@
 use std::iter;
 
+use cgmath::{InnerSpace, Rotation3, Zero};
 use wgpu::util::DeviceExt;
 use winit::{
   event::*,
@@ -8,6 +9,76 @@ use winit::{
 };
 
 mod texture;
+
+const NUM_INSTANCES_PER_ROW: u32 = 10;
+const NUM_INSTANCES: u32 = NUM_INSTANCES_PER_ROW * NUM_INSTANCES_PER_ROW;
+const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(
+  NUM_INSTANCES_PER_ROW as f32 * 0.5,
+  0.0,
+  NUM_INSTANCES_PER_ROW as f32 * 0.5,
+);
+
+struct Instance {
+  position: cgmath::Vector3<f32>,
+  rotation: cgmath::Quaternion<f32>,
+}
+
+impl Instance {
+  fn to_raw(&self) -> InstanceRaw {
+    InstanceRaw {
+      model: (cgmath::Matrix4::from_translation(self.position)
+        * cgmath::Matrix4::from(self.rotation))
+      .into(),
+    }
+  }
+}
+
+// NEW!
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct InstanceRaw {
+  model: [[f32; 4]; 4],
+}
+
+impl InstanceRaw {
+  fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+    use std::mem;
+    wgpu::VertexBufferLayout {
+      array_stride: mem::size_of::<InstanceRaw>() as wgpu::BufferAddress,
+      // We need to switch from using a step mode of Vertex to Instance
+      // This means that our shaders will only change to use the next
+      // instance when the shader starts processing a new instance
+      step_mode: wgpu::InputStepMode::Instance,
+      attributes: &[
+        wgpu::VertexAttribute {
+          offset: 0,
+          // While our vertex shader only uses locations 0, and 1 now, in later tutorials we'll
+          // be using 2, 3, and 4, for Vertex. We'll start at slot 5 not conflict with them later
+          shader_location: 5,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+        // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
+        // for each vec4. We'll have to reassemble the mat4 in
+        // the shader.
+        wgpu::VertexAttribute {
+          offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
+          shader_location: 6,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+        wgpu::VertexAttribute {
+          offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+          shader_location: 7,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+        wgpu::VertexAttribute {
+          offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
+          shader_location: 8,
+          format: wgpu::VertexFormat::Float32x4,
+        },
+      ],
+    }
+  }
+}
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
@@ -136,6 +207,8 @@ struct State {
   uniforms: Uniforms,
   uniform_buffer: wgpu::Buffer,
   uniform_bind_group: wgpu::BindGroup,
+  instances: Vec<Instance>,
+  instance_buffer: wgpu::Buffer,
 }
 
 impl State {
@@ -166,6 +239,35 @@ impl State {
       )
       .await
       .unwrap();
+
+    let instances = (0..NUM_INSTANCES_PER_ROW)
+      .flat_map(|z| {
+        (0..NUM_INSTANCES_PER_ROW).map(move |x| {
+          let position = cgmath::Vector3 {
+            x: x as f32,
+            y: 0.0,
+            z: z as f32,
+          } - INSTANCE_DISPLACEMENT;
+
+          let rotation = if position.is_zero() {
+            // this is needed so an object at (0, 0, 0) won't get scaled to zero
+            // as Quaternions can effect scale if they're not created correctly
+            cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), cgmath::Deg(0.0))
+          } else {
+            cgmath::Quaternion::from_axis_angle(position.clone().normalize(), cgmath::Deg(45.0))
+          };
+
+          Instance { position, rotation }
+        })
+      })
+      .collect::<Vec<_>>();
+
+    let instance_data = instances.iter().map(Instance::to_raw).collect::<Vec<_>>();
+    let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+      label: Some("Instance Buffer"),
+      contents: bytemuck::cast_slice(&instance_data),
+      usage: wgpu::BufferUsage::VERTEX,
+    });
 
     let sc_desc = wgpu::SwapChainDescriptor {
       usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
@@ -284,7 +386,7 @@ impl State {
       vertex: wgpu::VertexState {
         module: &shader,
         entry_point: "main",
-        buffers: &[Vertex::desc()],
+        buffers: &[Vertex::desc(), InstanceRaw::desc()],
       },
       fragment: Some(wgpu::FragmentState {
         module: &shader,
@@ -348,6 +450,8 @@ impl State {
       uniforms,
       uniform_buffer,
       uniform_bind_group,
+      instances,
+      instance_buffer,
     }
   }
 
@@ -405,8 +509,9 @@ impl State {
       render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
       render_pass.set_bind_group(1, &self.uniform_bind_group, &[]);
       render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+      render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
       render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-      render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+      render_pass.draw_indexed(0..self.num_indices, 0, 0..self.instances.len() as _);
     }
 
     self.queue.submit(iter::once(encoder.finish()));
