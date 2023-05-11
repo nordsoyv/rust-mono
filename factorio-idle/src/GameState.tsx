8f7b5ba9ad5cff2597b9edcab01b5d612400c4fa
Øyvind Nordsveen <oyvind.nordsveen@forsta.com>
2023-05-11 12:44:23 +0200

    Prettier + format code

--- factorio-idle/src/GameState.tsx
+++ factorio-idle/src/GameState.tsx
@@ -1,26 +1,26 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 //import {GameState} from "game-core";
-import {App} from "./App.tsx";
-import {GameState} from "../game-core/pkg/game_core";
+import { App } from './App.tsx';
+import { GameState } from '../game-core/pkg/game_core';
 
-
-function useForceUpdate(){
-    const [_value, setValue] = useState(0); // integer state
-    return () => setValue(value => ++value); // update the state to force render
+function useForceUpdate() {
+  const [_value, setValue] = useState(0); // integer state
+  return () => setValue((value) => ++value); // update the state to force render
 }
-export const GameStateContext = ({gameState}: {gameState:GameState})=> {
-    const forceUpdate = useForceUpdate()
-    useEffect(()=> {
-        setInterval(() => {
-            if (gameState != null) {
-                gameState.tick(1);
-                forceUpdate();
-            }
-        }, 100)
+export const GameStateContext = ({ gameState }: { gameState: GameState }) => {
+  const forceUpdate = useForceUpdate();
+  useEffect(() => {
+    setInterval(() => {
+      if (gameState != null) {
+        gameState.tick(1);
+        forceUpdate();
+      }
+    }, 100);
+  }, []);
 
-    },[])
-
-    return <React.StrictMode>
-        <App gameState={gameState}/>
+  return (
+    <React.StrictMode>
+      <App gameState={gameState} />
     </React.StrictMode>
-}
+  );
+};
--- factorio-idle/src/main.tsx
+++ factorio-idle/src/main.tsx
@@ -1,19 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import init,{GameState} from 'game-core';
-import {GameStateContext} from "./GameState.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import init, { GameState } from 'game-core';
+import { GameStateContext } from './GameState.tsx';
 
-let gameState : GameState;
-init().then(() => {
-    console.log("initialized");
+let gameState: GameState;
+init()
+  .then(() => {
+    console.log('initialized');
     gameState = GameState.new();
-}).then(() => {
+  })
+  .then(() => {
     ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-      <GameStateContext gameState={gameState}/>,
-    )
-})
-
-
+      <GameStateContext gameState={gameState} />,
+    );
+  });
