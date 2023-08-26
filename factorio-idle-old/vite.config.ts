b7510f1b7fb9d7c414c1a16a97dc1f07b72fa341
Øyvind <oyvind.nordsveen@gmail.com>
2023-08-26 11:09:17 +0200

    remove old stuff

--- factorio-idle-old/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import wasmPack from 'vite-plugin-wasm-pack';
-import react from '@vitejs/plugin-react-swc'
-export default defineConfig({
-  build: {
-    minify: false
-  },
-  plugins: [react(), wasmPack(['./game-core'])]
-});
