4ad2855c17ae8c8cb30ebecdbf2e203bf92cd2d5
Øyvind Nordsveen <oyvind.nordsveen@forsta.com>
2023-11-23 09:39:38 +0100

    Add benchmarking

---
+++ cdl-compiler/cdl-lexer/benches/benchmarks.rs
@@ -0,0 +1,11 @@
+use cdl_lexer::lex;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn criterion_benchmark(c: &mut Criterion) {
+  let file = include_str!("../test_script/test.cdl");
+
+  c.bench_function("parse large file (5k lines, 130kb)", |b| b.iter(|| lex(black_box(file))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
