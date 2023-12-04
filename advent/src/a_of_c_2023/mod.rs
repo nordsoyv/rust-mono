fa58f0399b4908a070761beb07737f8887f54bcd
Øyvind Nordsveen <oyvind.nordsveen@forsta.com>
2023-12-04 10:41:05 +0100

    task 04 a

--- advent/src/a_of_c_2023/mod.rs
+++ advent/src/a_of_c_2023/mod.rs
@@ -1,10 +1,12 @@
 mod task01;
 mod task02;
 mod task03;
+mod task04;
 
 use self::task01::{Task01A, Task01B};
 use self::task02::{Task02A, Task02B};
 use self::task03::{Task03A, Task03B};
+use self::task04::{Task04A, Task04B};
 use crate::task::Task;
 
 pub fn create_2023_task(id: &str) -> Box<dyn Task> {
@@ -15,6 +17,8 @@ pub fn create_2023_task(id: &str) -> Box<dyn Task> {
     "02b" => Box::new(Task02B {}),
     "03a" => Box::new(Task03A {}),
     "03b" => Box::new(Task03B {}),
+    "04a" => Box::new(Task04A {}),
+    "04b" => Box::new(Task04B {}),
     _ => panic!("No task found"),
   }
 }
---
+++ advent/src/a_of_c_2023/task04.rs
@@ -0,0 +1,106 @@
+use crate::{task::Task, util::read_file};
+
+pub struct Task04A {}
+pub struct Task04B {}
+
+impl Task for Task04A {
+  fn run(&self) {
+    let input = read_file("./res/2023/task04a.txt");
+    let cards: Vec<Card> = input
+      .lines()
+      .into_iter()
+      .map(|line| Card::from_string(line))
+      .collect();
+    let sum : i32= cards.into_iter().map(|c| c.calc_winning_value()).sum();
+    println!("The result is {}", sum);
+  }
+}
+
+impl Task for Task04B {
+  fn run(&self) {
+    println!("Emtpy task, not impemented yet");
+  }
+}
+
+#[derive(Debug, Default)]
+struct Card {
+  card_number: i32,
+  winning_numbers: Vec<i32>,
+  numbers: Vec<i32>,
+}
+
+impl Card {
+  fn from_string(input: &str) -> Card {
+    let parts: Vec<&str> = input.split(":").collect();
+    let card_num_par = parts[0];
+    let card_number: i32 = card_num_par.replace("Card ", "").trim().parse().unwrap();
+    let number_parts: Vec<&str> = parts[1].split("|").collect();
+
+    let winning_numbers = number_parts[0]
+      .trim()
+      .split(" ")
+      .collect::<Vec<&str>>()
+      .into_iter()
+      .filter(|number| number.len() > 0)
+      .map(|number| number.trim().parse::<i32>().unwrap())
+      .collect::<Vec<i32>>();
+    let numbers = number_parts[1]
+      .trim()
+      .split(" ")
+      .collect::<Vec<&str>>()
+      .into_iter()
+      .filter(|number| number.len() > 0)
+      .map(|number| number.trim().parse::<i32>().unwrap())
+      .collect::<Vec<i32>>();
+
+    Card {
+      card_number,
+      winning_numbers,
+      numbers,
+    }
+  }
+
+  fn calc_winning_value(&self) -> i32 {
+    let num_winning = self
+      .numbers
+      .clone()
+      .into_iter()
+      .filter(|number| self.winning_numbers.contains(number))
+      .collect::<Vec<i32>>()
+      .len();
+    if num_winning == 0 {
+      return 0;
+    }
+    if num_winning == 1 {
+      return 1;
+    }
+    return 2_i32.pow((num_winning - 1) as u32);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn task_a_example() {
+    let input = r"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    let cards: Vec<Card> = input
+      .lines()
+      .into_iter()
+      .map(|line| Card::from_string(line))
+      .collect();
+    assert_eq!(8, cards[0].calc_winning_value());
+    assert_eq!(2, cards[1].calc_winning_value());
+    assert_eq!(2, cards[2].calc_winning_value());
+    assert_eq!(1, cards[3].calc_winning_value());
+    assert_eq!(0, cards[4].calc_winning_value());
+    assert_eq!(0, cards[5].calc_winning_value());
+  }
+}
