0dda906b032906d43c10c16b11b258988e6a84b7
Øyvind Nordsveen <oyvind.nordsveen@forsta.com>
2024-01-04 12:23:13 +0100

    update benchmark

--- cdl-compiler/cdl-lexer/benches/benchmarks.rs
+++ cdl-compiler/cdl-lexer/benches/benchmarks.rs
@@ -4,7 +4,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 fn criterion_benchmark(c: &mut Criterion) {
   let file = include_str!("../../test_script/test.cdl");
 
-  c.bench_function("lex large file (5k lines, 130kb)", |b| b.iter(|| lex(black_box(file))));
+  c.bench_function("lex large file (30k lines, 780kb)", |b| b.iter(|| lex(black_box(file))));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- cdl-compiler/cdl-parser/benches/benchmarks.rs
+++ cdl-compiler/cdl-parser/benches/benchmarks.rs
@@ -4,7 +4,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 fn criterion_benchmark(c: &mut Criterion) {
   let file = include_str!("../../test_script/test.cdl");
 
-  c.bench_function("parse large file (5k lines, 130kb)", |b| b.iter(|| parse_text(black_box(file))));
+  c.bench_function("parse large file (30k lines, 130kb)", |b| b.iter(|| parse_text(black_box(file))));
 }
 
 criterion_group!(benches, criterion_benchmark);
