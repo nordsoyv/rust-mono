59d2a48e5b00948fe97219693e615ca3fdc66af4
Øyvind Nordsveen <oyvind.nordsveen@gmail.com>
2024-05-01 20:54:17 +0200

    macroquad

--- colony-game/game/src/main.rs
+++ colony-game/game/src/main.rs
@@ -1,86 +1,49 @@
-//! Basic hello world example.
+use macroquad::prelude::*;
 
-use ggez::{event, graphics, Context, GameResult};
-use std::{env, path};
-use ggez::conf::WindowSetup;
-
-// First we make a structure to contain the game's state
-struct MainState {
-  frames: usize,
+struct TileDrawer {
+  tiles: Texture2D,
 }
 
-impl MainState {
-  fn new(ctx: &mut Context) -> GameResult<MainState> {
-    ctx.gfx.add_font(
-      "LiberationMono",
-      graphics::FontData::from_path(ctx, "/LiberationMono-Regular.ttf")?,
-    );
+impl TileDrawer {
+  pub async fn new(path: &str) -> TileDrawer {
+    let tiles = load_texture(path).await.unwrap();
+    TileDrawer { tiles }
+  }
 
-    let s = MainState { frames: 0 };
-    Ok(s)
+  pub fn draw_tile(&self, pos_x: f32, pos_y: f32, x: f32, y: f32) {
+    draw_texture_ex(
+      &self.tiles,
+      pos_x,
+      pos_y,
+      WHITE,
+      DrawTextureParams {
+        dest_size: Some(Vec2 { x: 16.0, y: 16.0 }),
+        source: Some(Rect {
+          x: x * 16.0,
+          y: y * 16.0,
+          w: 16.0,
+          h: 16.0,
+        }),
+        ..Default::default()
+      },
+    );
   }
 }
 
-// Then we implement the `ggez:event::EventHandler` trait on it, which
-// requires callbacks for updating and drawing the game state each frame.
-//
-// The `EventHandler` trait also contains callbacks for event handling
-// that you can override if you wish, but the defaults are fine.
-impl event::EventHandler<ggez::GameError> for MainState {
-  fn update(&mut self, _ctx: &mut Context) -> GameResult {
-    Ok(())
-  }
+#[macroquad::main("Colony")]
+async fn main() {
+  let tile_drawer = TileDrawer::new("game/resources/16x16_terrain.png").await;
+  loop {
+    clear_background(BLACK);
 
-  fn draw(&mut self, ctx: &mut Context) -> GameResult {
-    let mut canvas =
-        graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
-
-    // Text is drawn from the top-left corner.
-    let offset = self.frames as f32 / 10.0;
-    let dest_point = ggez::glam::Vec2::new(offset, offset+1.0);
-    canvas.draw(
-      graphics::Text::new("Hello, world!")
-          .set_font("LiberationMono")
-          .set_scale(48.),
-      dest_point,
-    );
-
-
-    
-
-    canvas.finish(ctx)?;
-
-    self.frames += 1;
-    if (self.frames % 100) == 0 {
-      println!("FPS: {}", ctx.time.fps());
+    // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+    // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+    // draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+    // draw_text("Hello", 20.0, 20.0, 20.0, DARKGRAY);
+    for pos_x in 0..30 {
+      tile_drawer.draw_tile(100.0 + (pos_x as f32 *16.0), 100.0, pos_x as f32, 10.0);        
     }
 
-    Ok(())
+    next_frame().await
   }
-}
-
-// Now our main function, which does three things:
-//
-// * First, create a new `ggez::ContextBuilder`
-// object which contains configuration info on things such
-// as screen resolution and window title.
-// * Second, create a `ggez::game::Game` object which will
-// do the work of creating our MainState and running our game.
-// * Then, just call `game.run()` which runs the `Game` mainloop.
-pub fn main() -> GameResult {
-  // We add the CARGO_MANIFEST_DIR/resources to the resource paths
-  // so that ggez will look in our cargo project directory for files.
-  let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
-    let mut path = path::PathBuf::from(manifest_dir);
-    path.push("resources");
-    path
-  } else {
-    path::PathBuf::from("./resources")
-  };
-
-  let cb = ggez::ContextBuilder::new("helloworld", "ggez").add_resource_path(resource_dir);
-  let (mut ctx, event_loop) = cb.build()?;
-
-  let state = MainState::new(&mut ctx)?;
-  event::run(ctx, event_loop, state)
 }
