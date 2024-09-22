f61b209ef4b400449567928909b6544124a9b64f
Øyvind Nordsveen <oyvind.nordsveen@gmail.com>
2024-09-22 14:47:31 +0200

    cleanup

--- land-test/src/constants.rs
+++
@@ -1,2 +0,0 @@
-pub const INITIAL_WIDTH: u32 = (1920 / 4) * 3;
-pub const INITIAL_HEIGHT: u32 = (1080 / 4) * 3;
--- wasm-compiler/csl/src/main.rs
+++
@@ -1,7 +0,0 @@
-use cli_args;
-
-fn main() {
-  let cli = cli_args::parse_args();
-  println!("name: {:?}", cli.file_name.as_deref());
-  println!("output: {:?}", cli.output.as_deref());
-}
