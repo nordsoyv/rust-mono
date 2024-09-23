96dccf15499db938aabc0798801b0540984770f6
Øyvind Nordsveen <oyvind.nordsveen@gmail.com>
2024-09-23 18:11:17 +0200

    next stage

--- wgpu-egui/src/lib.rs
+++ wgpu-egui/src/lib.rs
@@ -1,3 +1,5 @@
+use std::char::decode_utf16;
+
 use wgpu::util::DeviceExt;
 use winit::{
   event::*,
@@ -27,18 +29,17 @@ impl Vertex {
 }
 // lib.rs
 const VERTICES: &[Vertex] = &[
-  Vertex {
-    position: [0.0, 0.5, 0.0],
-    color: [1.0, 0.0, 0.0],
-  },
-  Vertex {
-    position: [-0.5, -0.5, 0.0],
-    color: [0.0, 1.0, 0.0],
-  },
-  Vertex {
-    position: [0.5, -0.5, 0.0],
-    color: [0.0, 0.0, 1.0],
-  },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
 ];
 
 struct State<'a> {
@@ -51,6 +52,8 @@ struct State<'a> {
   vertex_buffer: wgpu::Buffer,
   window: &'a Window,
   num_vertices: u32,
+num_indices: u32,
+index_buffer: wgpu::Buffer,
 }
 
 impl<'a> State<'a> {
@@ -171,6 +174,12 @@ impl<'a> State<'a> {
       usage: wgpu::BufferUsages::VERTEX,
     });
 
+    let index_buffer  = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Index buffer"),
+        contents: bytemuck::cast_slice(INDICES),
+        usage: wgpu::BufferUsages::INDEX,
+    });
+
     Self {
       window,
       surface,
@@ -181,6 +190,8 @@ impl<'a> State<'a> {
       render_pipeline,
       vertex_buffer,
       num_vertices: VERTICES.len() as u32,
+      index_buffer,
+      num_indices: INDICES.len() as u32,
     }
   }
 
@@ -240,7 +251,8 @@ impl<'a> State<'a> {
       });
       render_pass.set_pipeline(&self.render_pipeline); // 2.
       render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-      render_pass.draw(0..self.num_vertices, 0..1); // 3.
+      render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+      render_pass.draw_indexed(0..self.num_indices,0, 0..1); // 3.
     }
 
     // submit will accept anything that implements IntoIter
