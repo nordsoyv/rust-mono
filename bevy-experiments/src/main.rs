43d45e59b60128d21700f23bc28933ba4c47cd8b
Øyvind Nordsveen <oyvind.nordsveen@forsta.com>
2025-03-17 14:06:03 +0100

    -

--- bevy-experiments/src/main.rs
+++ bevy-experiments/src/main.rs
@@ -1,24 +1,47 @@
 use bevy::{
-  color::{self, palettes::css::LIME}, pbr::wireframe::{NoWireframe, Wireframe, WireframeColor, WireframeConfig, WireframePlugin}, prelude::*, render::{
-    settings::{WgpuFeatures, WgpuSettings}, RenderPlugin
-  }
+  color::{self, palettes::css::LIME},
+  pbr::wireframe::{NoWireframe, Wireframe, WireframeColor, WireframeConfig, WireframePlugin},
+  prelude::*,
+  render::{
+    RenderPlugin,
+    mesh::VertexAttributeValues,
+    settings::{WgpuFeatures, WgpuSettings},
+  },
 };
+use bevy_egui::{EguiContexts, EguiPlugin, egui};
+
 #[derive(Component)]
 struct MyCameraMarker;
 
-fn setup_camera(
+#[derive(Component)]
+struct PlaneMarker;
+
+#[derive(Resource)]
+struct UiResource {
+  pub value: f32,
+}
+
+fn setup(
   mut commands: Commands,
   mut meshes: ResMut<Assets<Mesh>>,
   mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
   commands.spawn((
-    Mesh3d(meshes.add(Plane3d::default().mesh().size(10.0, 10.0).subdivisions(20).normal(Dir3::Z))),
+    Mesh3d(
+      meshes.add(
+        Plane3d::default()
+          .mesh()
+          .size(10.0, 10.0)
+          .subdivisions(20)
+          .normal(Dir3::Z),
+      ),
+    ),
     MeshMaterial3d(materials.add(Color::from(color::palettes::css::ALICE_BLUE))),
     Transform::from_xyz(0.0, 0.0, 0.0)
       .with_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-    //Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
     Wireframe,
-    WireframeColor{ color: LIME.into()}
+    WireframeColor { color: LIME.into() },
+    PlaneMarker,
   ));
   // cube
   commands.spawn((
@@ -43,6 +66,31 @@ fn setup_camera(
   ));
 }
 
+fn ui_example_system(mut contexts: EguiContexts, mut ui_resource: ResMut<UiResource>) {
+  egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+    ui.add(egui::Slider::new(&mut ui_resource.value, 0.0..=100.0).text("My value"))
+  });
+}
+
+fn update_plane(
+  ui_resource: Res<UiResource>,
+  query: Query<&Mesh3d, With<PlaneMarker>>,
+  mut meshes: ResMut<Assets<Mesh>>,
+) {
+  let mesh_handle = query.get_single().unwrap();
+  let mesh = meshes.get_mut(mesh_handle).unwrap();
+  let vertices = mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION).unwrap();
+  let VertexAttributeValues::Float32x3(vertices) = vertices else {
+    panic!("Unexpected vertex format, expected Float32x3.");
+  };
+  let mut index = 0;
+  for vertex in vertices.iter_mut() {
+    if index == 50 {
+      vertex[2] = ui_resource.value / 10.0;
+    }
+    index += 1;
+  }
+}
 
 fn main() {
   App::new()
@@ -55,7 +103,10 @@ fn main() {
         ..default()
       }),
       WireframePlugin,
+      EguiPlugin,
     ))
-    .add_systems(Startup, setup_camera)
+    .insert_resource(UiResource { value: 10.0 })
+    .add_systems(Startup, setup)
+    .add_systems(Update, (ui_example_system, update_plane).chain())
     .run();
 }
